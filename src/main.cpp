d647df1f33dbbdd48f742b5ac9d2a5a2965df062
Brian <brian.delvgne@gmail.com>
2024-03-22 15:23:43 +0100

    ajout imgui

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,35 @@ int main(){
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // On définit le callback à appeler lors du redimensionnement de la fenêtre
     glClearColor(0.09f,0.27f,0.22f,1.0f); 
 
+    // Setup ImGui binding
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    ImGui::StyleColorsDark();
+
     // Boucle de rendu
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
         processInput(window);
+
+        // Start the ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.0f, 0.0f, 0.0f, 0.75f)); // changer la couleur du fond de la fenêtre imgui
+        // Create your ImGui window here
+        ImGui::Begin("Panneau de contrôle");
+
+        ImGui::PopStyleColor(); // Restaurer la couleur de fond par défaut après la fin de la fenêtre important!!!!
+
+        ImGui::End();
+
+
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
